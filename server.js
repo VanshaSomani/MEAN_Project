d89627ff256818274e95fcd41e49fb1bbfc79d26
VanshaSomani <113380811+VanshaSomani@users.noreply.github.com>
2023-07-04 14:04:52 +0530

    server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+require("./app/src/config/dbConfig").getDbConnection()
+
+const categoryRoutes = require("./app/src/routes/category.routes")
+const productRoutes = require("./app/src/routes/product.routes")
+const publicRoutes = require("./app/src/routes/public.routes")
+const authMiddlerware = require("./app/src/middleware/auth.middleware")
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+ 
+app.use("/admin",authMiddlerware,categoryRoutes)
+app.use("/admin",authMiddlerware,productRoutes)
+
+app.use("/public",publicRoutes)
+
+
+app.listen(9999)
+console.log("server started 9999");
